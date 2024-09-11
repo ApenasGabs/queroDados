ab00c2a344f0cb004cf14217e14f3855f88f38de
Gabriel Rodrigues <gabers357@gmail.com>
2024-09-10 22:44:08 -0300

    Inciando paginacao

--- src/index.js
+++ src/index.js
@@ -1,57 +1,74 @@
 const puppeteer = require("puppeteer");
-const targetURl =
-  "https://www.olx.com.br/imoveis/venda/casas/estado-sp/grande-campinas/campinas?q=casas&sp=1s";
+const targetURL =
+  "https://www.olx.com.br/imoveis/venda/casas/estado-sp/grande-campinas/campinas?q=casas&sp=1";
 const propertyType = "casas";
 const propertyMaxPrice = 500000;
-// const propertyType = "casas" | "apartamentos";
-const targetURlWithPrice = `https://www.olx.com.br/imoveis/venda/casas/estado-sp/grande-campinas/campinas?f=p&pe=${propertyMaxPrice}&q=${propertyType}&sp=1`;
+const targetURLWithPrice = `https://www.olx.com.br/imoveis/venda/casas/estado-sp/grande-campinas/campinas?f=p&pe=${propertyMaxPrice}&q=${propertyType}&sp=1`;
+
+const getHouseList = async (page) => {
+  return await page.evaluate(() => {
+    const filteredItems = Array.from(
+      document.querySelectorAll(
+        'section[data-ds-component="DS-AdCard"].olx-ad-card--horizontal'
+      )
+    );
+    return filteredItems.map((li) => {
+      const price = li.querySelector(
+        'h3[data-ds-component="DS-Text"]'
+      )?.innerText;
+
+      const address = li.querySelector(
+        ".olx-ad-card__location-date-container > p"
+      )?.innerText;
+
+      const bedrooms = li.querySelector(
+        'ul[data-testid="labelGroup"] li span'
+      )?.innerText;
+
+      return { address, bedrooms, price };
+    });
+  });
+};
+
+const goToNextPage = async (page) => {
+  const nextButtonSelector = document.querySelector(
+    'div#listing-pagination a[aria-label="Próxima página"]'
+  );
+  const nextButton = await page.click(nextButtonSelector);
+  if (nextButton) {
+    await Promise.all([
+      // page.waitForNavigation({ waitUntil: "networkidle0" }),
+      page.click(nextButtonSelector),
+    ]);
+    return true;
+  }
+  return false;
+};
 
 const main = async () => {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    // userDataDir: "/tmp/myChromeSession",
   });
-  debugger;
-  const page = await browser.newPage();
   const houseList = [];
+  const page = await browser.newPage();
+
   try {
-    await page.goto(targetURlWithPrice);
-    // await page.goto(targetURlWithPrice, { waitUntil: "networkidle0" });
+    await page.goto(targetURLWithPrice);
 
-    const houseList = await page.evaluate(() => {
-      // Seleciona os elementos desejados
-      const filteredItens = Array.from(
-        document.querySelectorAll(
-          'section[data-ds-component="DS-AdCard"].olx-ad-card--horizontal'
-        )
-      );
+    let hasNextPage = true;
+    while (hasNextPage) {
+      const newHouses = await getHouseList(page);
+      houseList.push(...newHouses);
+      console.log("houseList: ", houseList);
 
-      return filteredItens.map((li) => {
-        const price = li.querySelector(
-          'h3[data-ds-component="DS-Text"]'
-        )?.innerText;
-        console.log("price: ", price);
-
-        const address = li.querySelector(
-          ".olx-ad-card__location-date-container > p"
-        )?.innerText;
-        console.log("address: ", address);
-
-        const bedrooms = li.querySelector(
-          'ul[data-testid="labelGroup"] li span'
-        )?.innerText;
-
-        console.log("bedrooms: ", bedrooms);
-        return { price, address, bedrooms };
-      });
-    });
-    console.log("houseList: ", houseList);
-    houseList.forEach((house) => {
-      console.log("house: ", house);
-    });
+      hasNextPage = await goToNextPage(page);
+    }
   } catch (error) {
     console.log("error: ", error);
+  } finally {
+    await browser.close();
   }
 };
+
 main();
