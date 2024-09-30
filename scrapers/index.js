765266a75d79d92f1ce4151ebdf6e8c422772ab3
Gabriel Rodrigues <gabers357@gmail.com>
2024-09-30 11:59:23 -0300

    🔧 Atualiza workflow de scrape para suportar OLX e Zap, refatorando a estrutura e adicionando tratamento de argumentos no scraper

--- scrapers/index.js
+++ scrapers/index.js
@@ -1,12 +1,20 @@
 const olxScraper = require("./olxScraper");
 const zapScraper = require("./zapScraper");
 
+const scraperType = process.argv[2];
+
 (async () => {
   try {
-    await olxScraper();
-    await zapScraper();
-    console.log("Todos os scrapers foram executados com sucesso.");
+    if (scraperType === "olx") {
+      await olxScraper();
+      console.log("OLX scraper executado com sucesso.");
+    } else if (scraperType === "zap") {
+      await zapScraper();
+      console.log("Zap scraper executado com sucesso.");
+    } else {
+      console.log("Por favor, passe 'olx' ou 'zap' como argumento.");
+    }
   } catch (error) {
-    console.error("Erro ao executar os scrapers:", error);
+    console.error("Erro ao executar o scraper:", error);
   }
 })();
